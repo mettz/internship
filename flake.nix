c64111e4a468a207d6d732c12da0c3ed8ae5b9e1
Mattia Guazzaloca <mattia.guazzaloca@gmail.com>
2025-06-04 10:37:38 +0200

    switched to unstable nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:brianmcgillion/nixpkgs/crazyflie-add";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
   outputs =
